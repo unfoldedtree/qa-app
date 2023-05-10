c725385993ac162ccb0e965e78b478f8c7bcd007
Will McMahan <wmcmahan14@gmail.com>
2023-05-10 10:39:33 -0500

    .

--- vueapp/env.d.ts
+++ vueapp/env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv extends Readonly<Record<string, string | boolean | undefined>> {
-    readonly VITE_EXCHANGE_API_URL: string,
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv
-}
--- vueapp/src/main.ts
+++ vueapp/src/main.ts
@@ -3,15 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css';
-// import axios from "axios";
 
 const app = createApp(App)
-
-// console.log(import.meta.env.DEV);
-
-// if (!import.meta.env.DEV) {
-//     axios.defaults.baseURL = import.meta.env.VITE_EXCHANGE_API_URL;
-// }
 
 app.use(router);
 
